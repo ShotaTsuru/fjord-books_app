6952034050481b90145411c725aaf902d63e38cf
ShotaTsuru <syota.crane@gmail.com>
2021-07-31 20:30:27 +0900

    icon投稿、表示機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: %i[user_name address introduction]
+      :sign_up, keys: %i[user_name address introduction icon]
     )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
   max_paginates_per 100
 
   has_many :books, dependent: :destroy
+  has_one_attached :icon
+
+  validate :icon_cannot_be_used
+
+  def icon_cannot_be_used
+    errors.add(:icon, 'に指定の画像は保存できません') unless ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'].include?(icon.blob.content_type)
+  end
 end

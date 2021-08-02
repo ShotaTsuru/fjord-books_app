4fcdfe76eebb87a004f99423293d8cac80b8758f
ShotaTsuru <syota.crane@gmail.com>
2021-08-02 13:32:53 +0900

    inverse_ofの定義修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :active_follow, dependent: :destroy, inverse_of: :user, # フォローする側の中間テーブルの定義
+  has_many :active_follow, dependent: :destroy, inverse_of: :following, # フォローする側の中間テーブルの定義
                            class_name: 'FollowRelationship',
                            foreign_key: 'following_id'
   has_many :followings, through: :active_follow, source: :follower
 
-  has_many :passive_follow, dependent: :destroy, inverse_of: :user, # フォローされている側の中間テーブルの定義
+  has_many :passive_follow, dependent: :destroy, inverse_of: :follower, # フォローされている側の中間テーブルの定義
                             class_name: 'FollowRelationship',
                             foreign_key: 'follower_id'
   has_many :followers, through: :passive_follow, source: :following

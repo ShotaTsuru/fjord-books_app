0c0563129e1ca0179d7e0c8b475b2594806adb35
ShotaTsuru <syota.crane@gmail.com>
2021-08-06 14:36:55 +0900

    icon画像投稿機能をactive_strageを使って実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_one_attached :icon
+
+  validate :icon_cannot_be_used
+
+  private
+
+  def icon_cannot_be_used
+    errors.add(:icon, 'に指定の画像は保存できません') unless ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'].include?(icon.blob.content_type)
+  end
 end

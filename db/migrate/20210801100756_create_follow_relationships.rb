683b1464540d8a4f14b0634660904c87e1993915
ShotaTsuru <syota.crane@gmail.com>
2021-08-08 08:03:19 +0900

    shemaを再migrationによって修正。follow_relationshipsのmigrationファイルにadd_indexをまとめる。user詳細のviewにて条件分岐を明示的なコードに書き換え。

--- db/migrate/20210801100756_create_follow_relationships.rb
+++ db/migrate/20210801100756_create_follow_relationships.rb
@@ -8,5 +8,6 @@ class CreateFollowRelationships < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :follow_relationships, %i[following_id follower_id], unique: true
   end
 end
--- db/migrate/20210802050314_add_index_following_id_and_follower_id_to_follow_relationships.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexFollowingIdAndFollowerIdToFollowRelationships < ActiveRecord::Migration[6.1]
-  def change
-    add_index :follow_relationships, %i[following_id follower_id], unique: true
-  end
-end
